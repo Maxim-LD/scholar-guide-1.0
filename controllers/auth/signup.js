0f89db75feeedeffa12da1a33e76b54a4e4c5f65
Maxim-LD <109170159+Maxim-LD@users.noreply.github.com>
2025-03-21 23:30:57 +0100

    Update signup.js

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs')
+const Users = require('../../models/User')
 const { asyncHandler } = require("../../middlewares/errorHandler")
 
 const signup = asyncHandler(async (req, res) => {
