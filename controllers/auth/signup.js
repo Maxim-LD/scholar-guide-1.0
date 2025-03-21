8e6dd7cda4de00aa35fada1720029add34c28d15
Maxim-LD <ayinde779@gmail.com>
2025-03-21 09:02:54 +0100

    Add signup controller for user registration

---
+++ controllers/auth/signup.js
@@ -0,0 +1,27 @@
+const { asyncHandler } = require("../../middlewares/errorHandler")
+
+const signup = asyncHandler(async (req, res) => {
+  const { email, password, role } = req.body
+  
+  if (!email || !password || !role) {
+    return res.status(400).json({
+        error: "All fields are required"
+    })
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 12)
+
+  const newUser = new Users({
+      email,
+      password: hashedPassword,
+      role,
+  })
+
+  await newUser.save()
+  return res.status(201).json({
+      message: "Account created succesfully!",
+      user: newUser,
+  })
+})
+
+module.exports = signup
