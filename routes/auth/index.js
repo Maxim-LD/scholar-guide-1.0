9d4add9f62b9cad130fa5c8628adc59a61afeaca
Maxim-LD <ayinde779@gmail.com>
2025-03-21 23:59:11 +0100

    Fix typo in signup controller import

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { validateUser } = require('../../validations/authValidator')
 const login = require('../../controllers/auth/login')
-const signup = require('../../controllers/auth/signUp')
+const signup = require('../../controllers/auth/signup')
 
 const authRouter = express.Router()
 
